e3ed602e3f341c3147c4dc6077c060d9d835e681
Azaki21421 <akasine@mail.ru>
2022-07-05 23:15:06 +0300

    Крах

---
+++ My project/Assets/MC/Stats.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    public float Start_HP = 100f;
+    public float Current_HP = 100f;
+    public float Attack_Speed = 1f;
+    public float Deffence = 20f;
+    public float Crit = 0f;
+    private float Crit_Chanse = 0f;
+    public float Speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
